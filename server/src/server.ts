efdc23d0d18961fedb5206f3c5f17c74e79e7d2f
Irine7 <irine7@yandex.ru>
2024-02-23 13:54:36 +0100

    feat: small changes in server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
-import express from "express";
-import { PrismaClient } from "@prisma/client";
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -7,24 +7,24 @@ const app = express();
 app.use(express.json());
 
 prisma.player.create({
-  data: {
-    player_name: "Michael",
-  },
+	data: {
+		player_name: 'Michael',
+	},
 });
 
 // Create a new Player
-app.post("/players", async (req, res) => {
-  const { name } = req.body;
-  try {
-    const player = await prisma.player.create({
-      data: {
-        player_name: name,
-      },
-    });
-    res.json(player);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to create player" });
-  }
+app.post('/players', async (req, res) => {
+	const { name } = req.body;
+	try {
+		const player = await prisma.player.create({
+			data: {
+				player_name: name,
+			},
+		});
+		res.json(player);
+	} catch (error) {
+		res.status(500).json({ error: 'Failed to create player' });
+	}
 });
 
-app.listen(3000, () => console.log("Server is on port", 3000));
+app.listen(3000, () => console.log('Server is on port', 3000));
