c03b803ecbfeef5d2de634f34b1f10c7bf2a2d10
Irine7 <irine7@yandex.ru>
2024-02-24 17:19:36 +0100

    feat: add routes, controllers & services

---
+++ server/src/routes/routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+
+import { DiceGameController } from '../controllers/DiceGameController';
+
+export const diceGameRouter = Router();
+
+// diceGameRouter.post('/players', diceGameController.createPlayer);
+diceGameRouter.get('/players', DiceGameController.getAllPlayers);
+// diceGameRouter.put('/players/:id', diceGameController.updatePlayer);
+
+// diceGameRouter.post('/games/:id', diceGameController.createGame);
+// diceGameRouter.put('/games/:id', diceGameController.updateGame);
+// diceGameRouter.delete('/games/:id', diceGameController.deleteGame);
+
+// diceGameRouter.get('/ranking', diceGameController.allRanking);
+// diceGameRouter.get('/ranking/loser', diceGameController.loserRanking);
+// diceGameRouter.get('/ranking/winner', diceGameController.winnerRanking);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,30 +1,9 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { diceGameRouter } from './routes/routes';
 
 const app = express();
 app.use(express.json());
 
-prisma.player.create({
-	data: {
-		player_name: 'Michael',
-	},
-});
-
-// Create a new Player
-app.post('/players', async (req, res) => {
-	const { name } = req.body;
-	try {
-		const player = await prisma.player.create({
-			data: {
-				player_name: name,
-			},
-		});
-		res.json(player);
-	} catch (error) {
-		res.status(500).json({ error: 'Failed to create player' });
-	}
-});
+app.use('/api', diceGameRouter);
 
 app.listen(3000, () => console.log('Server is on port', 3000));
