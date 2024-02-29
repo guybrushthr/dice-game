f60740ae6e004f67966f4780145952381251230c
Irine7 <irine7@yandex.ru>
2024-02-29 19:05:55 +0100

    feat: add initial front part & styles

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<section className="wrapper">
+			<h3 className="title">Dice Game</h3>
+			<div className="player-form">
+				<div className="dice">Enter your name:</div>
+				<input type="text" id="name" />
+				<button className="btn-submit">Submit</button>
+			</div>
+			<div className="dice-form">
+				<button className="btn-roll">Roll</button>
+				<div className="result">Result:</div>
+			</div>
+			<div className="ranking">
+				<p className="player">Players Ranking</p>
+				<div className="ranking-list">
+					<button className="rank">All Players</button>
+					<button className="player">Ranking</button>
+					<button className="points">Best Player</button>
+					<button className="points">Worst Player</button>
+				</div>
+				<div className="ranking-table">
+					<p className="rank">ID</p>
+					<p className="player">Name</p>
+					<p className="points">Success Percentage</p>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default App;
